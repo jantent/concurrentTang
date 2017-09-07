978850e657232fadb4c8cdf94b0f2975a82d9664
tangj <tangj@koal.com>
2017-09-07 15:02:02 +0800

    第一个实例

--- src/main/java/com/chapter1/FirstRunnable.java
+++ src/main/java/com/chapter1/FirstRunnable.java
@@ -1,18 +1,11 @@
 package com.chapter1;
 
-/**
- * 
- * @author tangj
- *
- * 第一个runnable线程
- */
 public class FirstRunnable {
 
 	public static void main(String[] args) {
-		MyRunnable myRunnable = new MyRunnable();
-		Thread thread = new Thread(myRunnable);
-		thread.start();
-		// myRunnable.run();
+		MyRunnable myRunnable;
+		myRunnable = new MyRunnable();
+		new Thread(myRunnable).start();
 	}
 
 }
@@ -23,5 +16,4 @@ class MyRunnable implements Runnable {
 	public void run() {
 		System.out.println("Hello World!");
 	}
-
 }
--- src/main/java/com/chapter1/FirstThread.java
+++ src/main/java/com/chapter1/FirstThread.java
@@ -1,24 +1,18 @@
 package com.chapter1;
 
-/**
- * 
- * @author tangj
- * 
- * 第一个简单的线程
- */
 public class FirstThread {
 
 	public static void main(String[] args) {
 		InnerThread thread = new InnerThread();
+		// 线程启动
 		thread.start();
-		System.out.println("thread stop");
 	}
 
-	
 }
 
-class InnerThread extends Thread{
+class InnerThread extends Thread {
 
+	// Override run方法，写入自己想要实现的业务
 	@Override
 	public void run() {
 		super.run();
