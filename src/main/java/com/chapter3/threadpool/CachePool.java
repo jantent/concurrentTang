152fdc09b24546191cfa9b26f99f247673583129
tangj <tangj@koal.com>
2017-09-11 16:46:16 +0800

    test

--- src/main/java/com/chapter3/threadpool/CachePool.java
+++ src/main/java/com/chapter3/threadpool/CachePool.java
@@ -4,6 +4,10 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程
+ * 线程池为无限大，当执行第二个任务时第一个任务已经完成，会复用执行第一个任务的线程，而不用每次新建线程。
+ */
 public class CachePool {
     private static Runnable getThread(final int i){
         return new Runnable() {
