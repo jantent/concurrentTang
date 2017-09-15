02641ab09f169d8b792f34a65b8fdc0307e15c7e
tangj <tangj@koal.com>
2017-09-15 16:57:29 +0800

    test

--- src/main/java/com/chapter3/threadpool/CachePool.java
+++ src/main/java/com/chapter3/threadpool/CachePool.java
@@ -13,6 +13,11 @@ public class CachePool {
         return new Runnable() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(4000);
+                }catch (Exception e){
+
+                }
                 System.out.println(i);
             }
         };
@@ -21,11 +26,6 @@ public class CachePool {
     public static  void main(String args[]){
         ExecutorService cachePool = Executors.newCachedThreadPool();
         for (int i=1;i<=10;i++){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             cachePool.execute(getThread(i));
         }
     }
